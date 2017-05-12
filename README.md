9587841d05703ad3363760972af33ed75a2cd1a9
Thom Kitchen <thomkitchen@gmail.com>
2017-05-12 15:13:48 -0400

    Updated code to PEP8 style. Changed readme slightly

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # yelp-review-scraper
 Web scraper that grabs all reviews for a business's yelp page and returns them as a string for parsing. Originally designed for a Natural Language Processing project
 ## Use
-Import module into project. Use the get_review() functions by feeding it the Yelp URL of the business/product you want to get the reviews for.
+Import module into project. Use the get_review() functions by feeding it the Yelp URL of the business/product you want to get the reviews for as a parameter.
+ex. get_review("http://yelp.url.of.business.com")
 ### Note
 Runs on HTML naming conventions of Yelp. If they change their HTML/CSS structure, update the code at the marked locations
--- yelpreviewscraper.py
+++ yelpreviewscraper.py
@@ -1,4 +1,4 @@
-#-------------------------------------------------------------------------------
+#
 # Name:        Yelp Review Scraper
 # Purpose:      Gather reviews from yelp page
 #
@@ -8,14 +8,13 @@
 # Last Modified: 03/26/2015
 # Copyright:   (c) thom 2014
 # Licence:     To Kill
-#-------------------------------------------------------------------------------
+#
 
 import urllib3
 import bs4
 import sys
 
 http = urllib3.PoolManager()
-
 
 
 def get_reviews(url):
@@ -25,12 +24,16 @@ def get_reviews(url):
         url: A url, pointing to the reviews for a product on yelp.
 
     Returns:
-        List[2]: First element is the product name.  Second element is a string containing all reviews.
+        List[2]: First element is the product name.
+                 Second element is a string containing all reviews.
     """
-#----------Empty string to hold reviews and bool to trigger cycle through pages of reviews
+# Empty string to hold reviews and bool to trigger
+# cycle through pages of reviews
     reviews = ''
     has_next_page = False
-#---------The calls to the web page and feeding those results into BeautifulSoup searching for reviews and next page button. Added exception handling
+# The calls to the web page and feeding those results into
+# BeautifulSoup searching for reviews and next page button.
+# Added exception handling
     try:
         response = http.request('GET', url)
     except urllib3.exceptions.HTTPError, e:
@@ -41,35 +44,38 @@ def get_reviews(url):
         return
 
     soup = bs4.BeautifulSoup(response.data)
-    next_page_button = soup.findAll('a', {"class" : "page-option prev-next next"})
-    review_content = soup.findAll('p', {"itemprop" : "description"})
+    next_page_button = soup.findAll('a',
+                                    {"class": "page-option prev-next next"})
+    review_content = soup.findAll('p', {"itemprop": "description"})
 
-#--------This routine grabs the product name to return in a list along with the reviews
+# Grabs the product name to return in a list along with the reviews
 
     product_name = soup.select('h1.biz-page-title')[0].text
 
 
-
-#-----Error check: CSS selector for reviews. may have changed
+# Error check: CSS selector for reviews. may have changed
     if(len(review_content)) == 0:
         print("An error has occured. No review content was found.")
         return
 
-#----Check if there is a next page button, if so trigger review cycling
+# Check if there is a next page button, if so trigger review cycling
     if len(next_page_button) != 0:
         has_next_page = True
 
-#-------Copies content of the CSS selector for reviews into a string object
+# Copies content of the CSS selector for reviews into a string object
     for node in review_content:
-            reviews += node.text
+        reviews += node.text
 
-#----Routine for handling multiple pages of reviews to concat all into a single string object
+# Routine for handling multiple pages of reviews
+# to concat all into a single string object
     page_num = 1
     while has_next_page:
         response = http.request('GET', (url + "?start=" + str(page_num*40)))
         soup = bs4.BeautifulSoup(response.data)
-        next_page_button = soup.findAll('a', {"class" : "page-option prev-next next"})
-        review_content = soup.findAll('p', {"itemprop" : "description"})
+        next_page_button = soup.findAll('a',
+                                        {"class":
+                                         "page-option prev-next next"})
+        review_content = soup.findAll('p', {"itemprop": "description"})
 
         for node in review_content:
             reviews += node.text + '\n\n'
@@ -79,10 +85,3 @@ def get_reviews(url):
         page_num = page_num + 1
 
     return product_name.strip(), reviews
-
-if __name__ == '__main__':
-
-    name, review = get_reviews("http://www.yelp.com/biz/brick-pizzeria-napoletana-new-bedford-2")
-    print(name + '\n\n' + review)
-
-
